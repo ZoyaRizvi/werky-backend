de246d99a67f97f73c876082f2859c8009663920
ZoyaRizvi <81320848+ZoyaRizvi@users.noreply.github.com>
2024-07-21 13:10:49 +0500

    updated

---
+++ routes/productRoute.js
@@ -0,0 +1,21 @@
+import express from 'express';
+
+import {
+  // createProduct,
+  // getProduct,
+  // getProducts,
+  // updateProduct,
+  // deleteProduct,
+  getProject,
+} from '../controllers/productController.js';
+
+const router = express.Router();
+
+router.get('/project/:id', getProject);
+// router.get('/', getProducts);
+// router.post('/new', createProduct);
+// router.get('/product/:id', getProduct);
+// router.put('/update/:id', updateProduct);
+// router.delete('/delete/:id', deleteProduct);
+
+export default router;
